cc300d296da61872e8bd7e1583e1caec28906a8f
JUAN MANUEL GALIDNO CASILLAS <casillasmanuel9@gmail.com>
2022-02-23 23:44:40 -0600

    modulos

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NoPageFoundComponent } from './pages/no-page-found/no-page-found.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router'
+
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)
+  },
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+  },
+  { path: '**', component: NoPageFoundComponent }
+]
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
+import { AuthModule } from './auth/auth.module';
+import { PagesModule } from './pages/pages.module';
+import { PagesComponent } from './pages/pages.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoPageFoundComponent } from './pages/no-page-found/no-page-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoPageFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
